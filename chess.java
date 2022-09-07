f60cf9a3d1cbcfac86b2a856a79bd2053e2dc492
madhumithagithub <112962387+madhumithagithub@users.noreply.github.com>
2022-09-07 15:12:27 +0530

    Add files via upload

---
+++ chess.java
@@ -0,0 +1,14 @@
+public class Main
+{
+	public static void main(String[] args) {
+	   long a=1,i;
+	   for(i=1;i<=64;i++)
+	   {
+	      a= a*2;
+	      	   System.out.println(a);
+	     
+	   }
+	   
+	}
+}
+
