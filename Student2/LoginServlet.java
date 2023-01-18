e4e43737db16e45e3a6994f32e60c735a841df57
madhumithagithub <112962387+madhumithagithub@users.noreply.github.com>
2023-01-18 11:30:53 +0530

    Create LoginServlet.java

---
+++ Student2/LoginServlet.java
@@ -0,0 +1,57 @@
+package Madhuitha;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String UserName=request.getParameter("UserName");
+		String Password=request.getParameter("Password");
+		try
+		{
+			if(UserName.equals("Admin") && Password.equals("Admin*10"))
+			{
+				response.sendRedirect("http://localhost:8081/Student2/TeacherButton.html");		
+			}
+			else
+			{
+				response.sendRedirect("http://localhost:8080/Student2/Login.html");
+			}
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
